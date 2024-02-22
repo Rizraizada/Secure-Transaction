85b8abbd1de4c361a38d2b85fa26c5a2d43241f3
Rizraizadda <rizraizada000@gmail.com>
2024-02-22 23:50:55 +0600

    auth

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use('/auth', authRoutes); // Mounting auth routes
 
+// Define a route handler for the root path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
